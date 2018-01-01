1947610ee910645a7421f560d9e9177c48b41a18
jssoscar <785979885jss@163.com>
2018-01-01 11:45:33 +0800

    增加mock代理

--- mock/index.js
+++ mock/index.js
@@ -13,12 +13,63 @@ const teacher = require('./teacher');
 
 const student = require('./student');
 
-const middleWare = (req, res, next) => {
+const yargs = require('yargs');
 
+const shouldProxy = !!yargs.argv.proxy;
+
+/**
+ * @author 熊建
+ * @email xiongjian@didichuxing.com
+ * @create date 2017-11-09 02:55:20
+ * @modify date 2017-11-09 02:55:20
+ * @desc [description]
+ */
+const request = require('request');
+const argv = require('yargs').argv;
+//是否设置代理
+const isProxy = !!argv.proxy;
+
+/**
+ * 配置代理，有的时候接口不走本地mock会走代理
+ * @param  {String} reqUrl
+ * @param  {Object} res
+ * @param  {Object} req
+ * @return {null}
+ */
+const doProxy = (reqUrl, res, req, next) => {
+    //https://www.npmjs.com/package/request
+    let handleError = error => {
+        if (!error) {
+            return;
+        }
+        console.error(error);
+        let errorText = error.message;
+        res.statusMessage = errorText;
+        res.writeHead(404, {
+            'Content-Type': 'text/plain'
+        });
+        res.write(errorText);
+        res.end();
+        console.log(reqUrl, '--------------------error-----------------------');
+    };
+    var iReq = request(reqUrl);
+    req.pipe(iReq)
+    iReq.pipe(res);
+    iReq.on('error', handleError);
+    // next();
+};
+
+const middleWare = (req, res, next) => {
     let url = req.url;
 
     if(/\./.test(url)){
         next();
+        return;
+    }
+
+    if(isProxy){
+        const reqUrl = `http://${argv.proxy.replace('http://', '').replace(/\/$/, '')}${url}`;
+        doProxy(reqUrl, res, req, next);
         return;
     }
 
